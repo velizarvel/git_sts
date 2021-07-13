0d4a72df1e9091ad74a4bd7ac4c0b7b926c8321d
Veljko Potparic <velizarvel@gmail.com>
2021-07-13 19:48:34 +0200

    first commit

---
+++ src/main/java/com/git/IktGitStsProjectApplication.java
@@ -0,0 +1,13 @@
+package com.git;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IktGitStsProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IktGitStsProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/git/IktGitStsProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.git;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IktGitStsProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
